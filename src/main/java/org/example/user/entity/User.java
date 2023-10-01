f76ffda2f265ad9b95254f48658a61b0dd8ea7d7
Moxirbek <moxirbek021199@gmail.com>
2023-10-01 13:30:47 +0500

    completed Adding user

--- src/main/java/org/example/user/entity/User.java
+++ src/main/java/org/example/user/entity/User.java
@@ -1,8 +1,19 @@
 package org.example.user.entity;
 
-import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.UUID;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class User {
     private UUID userId;
-
+    private String name;
+    private String surname;
+    private String password;
+    private String username;
+    private List<UUID> examResults;
 }
