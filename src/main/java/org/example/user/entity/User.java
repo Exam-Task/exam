faf6777a2b56b7fe1ac53d1e2a3688d4e6a8022b
Moxirbek <moxirbek021199@gmail.com>
2023-10-01 13:23:01 +0500

    added user

---
+++ src/main/java/org/example/user/entity/User.java
@@ -0,0 +1,8 @@
+package org.example.user.entity;
+
+import java.util.UUID;
+
+public class User {
+    private UUID userId;
+
+}
---
+++ src/main/java/org/example/user/entity/UserType.java
@@ -0,0 +1,5 @@
+package org.example.user.entity;
+
+public enum UserType {
+    ADMIN, USER
+}
